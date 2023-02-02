0545f5b0429929f55ae708fae5c697a83ecd6a2f
vladoslatinos <116588242+vladoslatinos@users.noreply.github.com>
2023-02-02 12:26:57 +0200

    Add files via upload

---
+++ Desktop/front-end/дз/15/js/index.js
@@ -0,0 +1,14 @@
+function getPalindrome(n) {
+  let steps = 0;
+  let current = n;
+  while (!isPalindrome(current)) {
+    steps++;
+    current = current + Number(current.toString().split('').reverse().join(''));
+  }
+  return {result: current, steps};
+}
+
+function isPalindrome(n) {
+  let str = n.toString();
+  return str === str.split('').reverse().join('');
+} console.log(getPalindrome(96))
