44523cd4d766da1532f7e90898d299f4b2b5d324
vladoslatinos <116588242+vladoslatinos@users.noreply.github.com>
2023-02-04 11:50:57 +0200

    Update index.js

--- Desktop/front-end/дз/20/js/index.js
+++ Desktop/front-end/дз/20/js/index.js
@@ -1,11 +1,47 @@
-function allSettled(promises) { 
-  return Promise.all(promises.map((promise) => { 
-    return promise 
-      .then((value) => { 
-        return { status: "fulfilled", value }; 
-      }) 
-      .catch((reason) => { 
-        return { status: "rejected", reason }; 
-      }); 
-  })); 
-};
+const promiseA = new Promise((resolve, reject) => setTimeout(() => resolve(5), 1000))
+const promiseB = new Promise((resolve, reject) => setTimeout(() => reject(1), 500))
+
+const promisesArray = [promiseA, promiseB]
+
+function allSettled(promises){
+
+    return new Promise((resolve, reject) => {
+        const settledPromises = []
+        let currentIndex = 0
+
+        const checkIfPromiseLast = (index) => {
+            if(index === (promises.length - 1)){
+                return true
+            }
+
+            return false
+        }
+
+        promises.forEach((promise) => {
+            promise
+            .then(() => {
+                if(checkIfPromiseLast(currentIndex)){
+                    resolve([...settledPromises, "fulfilled"])
+                }
+                else{
+                    currentIndex++
+                    settledPromises.push("fulfilled")
+                }
+            })
+            .catch(() => {
+                if(checkIfPromiseLast(currentIndex)){
+                    resolve([...settledPromises, "rejected"])
+                }
+                else{
+                    currentIndex++
+                    settledPromises.push("rejected")
+                }
+            })
+        })
+    })
+}
+
+allSettled(promisesArray)
+.then((results) => {
+    results.map((result) => console.log(result))
+})
