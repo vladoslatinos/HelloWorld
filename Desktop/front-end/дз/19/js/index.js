f3ad14748e343e2c2e018dfab150423412bf15cf
vladoslatinos <116588242+vladoslatinos@users.noreply.github.com>
2023-03-21 00:11:52 +0200

    Update index.js

--- Desktop/front-end/дз/19/js/index.js
+++ Desktop/front-end/дз/19/js/index.js
@@ -1,84 +1,53 @@
-const menuList = { 
-    burger: [ 
-        { 
-            name: '🍔', 
-            time: 3000 
-        }, 
-        { 
-            name: '🍟', 
-            time: 1000 
-        }, 
-        { 
-            name: '🥤', 
-            time: 500 
-        } 
-    ], 
-    chicken: [ 
-        { 
-            name: '🍗', 
-            time: 5000 
-        }, 
-        { 
-            name: '🥔', 
-            time: 2000 
-        }, 
-        { 
-            name: '🍺', 
-            time: 1500 
-        } 
-    ], 
-    avocado: [ 
-        { 
-            name: '🥑', 
-            time: 3000 
-        }, 
-        { 
-            name: '🍚', 
-            time: 5000 
-        }, 
-        { 
-            name: '🧉', 
-            time: 2000 
-        } 
-    ] 
-}; 
-
-function createOrder(menuItem, getResult) { 
-    const result = []; 
-
-    function getDish(menuItem, callback) { 
-        menuItem.forEach(({ name, time }, index) => { 
-            setTimeout(() => { 
-                if (Math.ceil(Math.random() * 10) < 8) { 
-                    const fulfilledResult = { 
-                        status: 'fulfilled', 
-                        value: name 
-                    } 
-
-                    callback(fulfilledResult, index); 
-                } else { 
-                    const rejectedResult = { 
-                        status: 'rejected', 
-                        reason: name 
-                    }; 
-
-                    callback(rejectedResult, index); 
-                } 
-            }, time) 
-        }) 
-    } 
-
-    function getDoneMenu(dish, index) { 
-        result[index] = dish; 
-
-        if (result.filter(item => item).length === menuItem.length) getResult(result); 
-    } 
-
-    getDish(menuItem, getDoneMenu); 
-} 
-
-createOrder(menuList.avocado, x => console.log(x)); 
-createOrder(menuList.burger, x => console.log(x)); 
-createOrder(menuList.chicken, x => console.log(x));
+const menuList = {  burger: [
+    {      name: '🍔',
+      time: 3000    },
+    {      name: '🍟',
+      time: 1000    },
+    {      name: '🥤',
+      time: 500    }
+  ],  chicken: [
+    {      name: '🍗',
+      time: 5000    },
+    {      name: '🥔',
+      time: 2000    },
+    {      name: '🍺',
+      time: 1500    }
+  ],  avocado: [
+    {      name: '🥑',
+      time: 3000    },
+    {
+      name: '🍚',      time: 5000
+    },    {
+      name: '🧉',      time: 2000
+    }  ]
+};
+function createOrder(menuItem) {  
+    console.log('Start cooking order...');
+  
+    const promises = menuItem.map(({ name, time }) => {
+    return new Promise((resolve, reject) => {      
+        setTimeout(() => {
+        const status = Math.random() < 0.8 ? 'fulfilled' : 'rejected';        
+        const result = {
+          status: status,          
+          value: name
+        };        
+            if (status === 'fulfilled') {
+                resolve(result);        
+            } else {
+                reject(result);        
+            }
+           }, time);    
+    });
+  });
+  return Promise.all(promises);
+}
+createOrder(menuList.chicken)
+  .then((res) => {    
+    console.log(res);
+  })  
+    .catch((err) => {
+    console.error(err);  
+});
 
 
